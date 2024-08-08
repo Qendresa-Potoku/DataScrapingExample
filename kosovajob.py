c347b6a529f7457f06e0ef9e54d64a0cdc76ae44
Qendresa-Potoku <94839915+Qendresa-Potoku@users.noreply.github.com>
2024-08-08 18:45:48 +0200

    Update kosovajob.py

--- kosovajob.py
+++ kosovajob.py
@@ -19,7 +19,7 @@ def fetch_and_update_job_listings():
         cursor = db.cursor()
 
         # Fetch the job listings page
-        url = 'https://kosovajob.com/rfq'
+        url = 'https://kosovajob.com/'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
